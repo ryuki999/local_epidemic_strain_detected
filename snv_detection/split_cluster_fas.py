62558a0fffe363ed010f1bafd0670e3fbb90d6f7
ryuki999 <ryuki999.1624@gmail.com>
2022-11-12 21:05:32 +0900

    [update]support parallel computation split_cluster

--- snv_detection/split_cluster_fas.py
+++ snv_detection/split_cluster_fas.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
 import os
+import re
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import time
 
@@ -47,38 +48,26 @@ if not Path(outputdir).exists():
     # continentまでのpath
     os.mkdir(Path(outputdir))
 
-# def create_sep_list(seq):
-    
-with open(FASTA_FILE, "r") as r:
-    # for i in tqdm(r.readlines()):
-    #     # print(i)
-    #     pass
-    seqs = [f">{i}" for i in r.read().split(">") if i != ""]
-    # seqs = [f">{i}" for i in tqdm(r.read().split(">")) if i != ""]
-
-# def write_seq(seq, ids, w):
-#     for id in ids:
-#         if id in seq[:100]:
-#             w.write(seq)
-#     #         return True
-#     # return False
-#     return
-
-seqs_h = [i.split("\n")[0] for i in seqs]
-
-for i, ids in enumerate(cluster_id):
+def create_file(params):
+    i, ids = params
+    header_pat = re.compile('^>(.*)')
+    is_target_section = False
     with open(f"{outputdir}/CLUSTER{i}_ID.fas", "w+") as w:
-        for i in range(len(seqs_h)):
-        # for i in tqdm(range(len(seqs_h))):
-            for id in ids:
-                if id in seqs_h[i]:
-                    w.write(seqs[i])
+        with open(FASTA_FILE, "r") as r:
+            for line in tqdm(r):
+                if m := header_pat.match(line):
+                    is_target_section = m[1] in ids
+                if is_target_section:
+                    w.write(line)
+    return
+
+# 並列処理
+with ProcessPoolExecutor(max_workers=len(cluster_id)) as executor:
+    params = enumerate(cluster_id)
+    results = executor.map(create_file, params)
+
 
 END_TIME = time.time()
 delta = END_TIME - START_TIME
 LOGGER.info(f"{CONTINENT} Done! 処理時間:{round(delta,3)}s\n")
-# with ProcessPoolExecutor(max_workers=6) as executor:
-#     for i, ids in enumerate(cluster_id):
-#         with open(f"{outputdir}/CLUSTER{i}_ID.fas", "w+") as w:
-#             for seq in tqdm(seqs):
-#                 executor.submit(write_seq, seq, ids, w)
+
